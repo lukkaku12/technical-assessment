2843a0d07659a2c98172ff936d0735b555265db1
Lucas <lucasaristiz789@gmail.com>
2025-06-14 14:25:43 -0500

    feat: implementation with jinja templates and db migrations

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from config.settings import db, DATABASE_URL
 from app.routes import inventory_bp
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="app/templates")
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
 app.register_blueprint(inventory_bp)
 
 if __name__ == "__main__":
